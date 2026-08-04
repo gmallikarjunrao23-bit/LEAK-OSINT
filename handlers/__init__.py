166ce3d6e13013464e2cc5f072123e7a4cbba17e
gmallikarjunrao23-bit <gmallikarjunrao23@gmail.com>
2026-08-04 21:40:21 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,11 @@
-
+# handlers/__init__.py
+from handlers import start
+from handlers import search
+from handlers import profile
+from handlers import plans
+from handlers import payment
+from handlers import refer
+from handlers import redeem
+from handlers import history
+from handlers import admin
+from handlers import support
