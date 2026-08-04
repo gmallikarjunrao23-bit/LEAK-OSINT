4629a9afc1d44f8e071c791e18c815872edb06e3
gmallikarjunrao23-bit <gmallikarjunrao23@gmail.com>
2026-08-04 19:41:28 +0530

    Create referral_repo.py

---
+++ repositories/referral_repo.py
@@ -0,0 +1,22 @@
+"""
+Referral repository
+"""
+
+from typing import Optional
+from sqlalchemy.ext.asyncio import AsyncSession
+from repositories.base import BaseRepository
+from models.referral import Referral
+
+class ReferralRepository(BaseRepository[Referral]):
+    def __init__(self, session: AsyncSession):
+        super().__init__(Referral, session)
+    
+    async def get_by_referred(self, referred_id: int) -> Optional[Referral]:
+        return await self.get(referred_id=referred_id)
+    
+    async def create_referral(self, referrer_id: int, referred_id: int, coins_awarded: int) -> Referral:
+        return await self.create(
+            referrer_id=referrer_id,
+            referred_id=referred_id,
+            coins_awarded=coins_awarded
+        )
