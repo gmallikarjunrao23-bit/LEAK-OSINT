cc146694489f23b30c569f319e56eae8e6fabdb6
gmallikarjunrao23-bit <gmallikarjunrao23@gmail.com>
2026-08-04 21:29:53 +0530

    Delete models/__init__.py

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-# models/__init__.py
-from models.base import BaseModel
-from models.user import User
-from models.query import Query
-from models.payment import Payment
-from models.referral import Referral
